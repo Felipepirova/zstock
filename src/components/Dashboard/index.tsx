5e30b7d019b04dbb3e95a7f2d5ced8b800082f94
Felipe <felipepirovano95@gmail.com>
2021-11-17 18:32:22 -0300

    front-end do dashboard

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,37 @@
+import { Container } from './styles'
+import moneyImg from '../../assets/money.svg'
+import arrowUpImg from '../../assets/arrow_up.svg'
+import arrowDownImg from '../../assets/arrow_down.svg'
+
+export function Dashboard() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Valor em caixa</p>
+          <img src={moneyImg} alt="Entradas" />
+        </header>
+        <strong className="money">
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(100000)}
+        </strong>
+      </div>
+      <div>
+        <header>
+          <p>Produtos em estoque</p>
+          <img src={arrowUpImg} alt="Saídas" />
+        </header>
+        <strong className="products">32 Unidades</strong>
+      </div>
+      <div>
+        <header>
+          <p>Produtos vendidos</p>
+          <img src={arrowDownImg} alt="Total" />
+        </header>
+        <strong className="sales">21 Unidades</strong>
+      </div>
+    </Container>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
+import { Dashboard } from '../../components/Dashboard'
 import { ProductList } from '../../components/ProductList'
 import { Container } from './style'
 
 export function Home() {
   return (
     <Container>
+      <Dashboard />
       <ProductList />
     </Container>
   )
