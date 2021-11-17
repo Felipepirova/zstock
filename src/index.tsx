bf670dc21fc166816a8287efcd331bd6012520c1
Felipe <felipepirovano95@gmail.com>
2021-11-17 15:03:55 -0300

    fake api retornando dados

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,77 @@
+import { createServer, Model } from 'miragejs'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+
+createServer({
+  models: {
+    products: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      products: [
+        {
+          id: 0,
+          description: 'Notebook',
+          type: 'eletronic',
+          saleValue: 1000,
+          profitValue: 1300,
+          amount: 3,
+          output: 1
+        },
+        {
+          id: 1,
+          description: 'Celular',
+          type: 'eletronic',
+          saleValue: 800,
+          profitValue: 1000,
+          amount: 5,
+          output: 1
+        },
+        {
+          id: 2,
+          description: 'Liquidificador',
+          type: 'appliances',
+          saleValue: 100,
+          profitValue: 300,
+          amount: 15,
+          output: 2
+        },
+        {
+          id: 3,
+          description: 'Sofa',
+          type: 'furniture',
+          saleValue: 1500,
+          profitValue: 3000,
+          amount: 2,
+          output: 0
+        },
+        {
+          id: 4,
+          description: 'Geladeira',
+          type: 'appliances',
+          saleValue: 1500,
+          profitValue: 2000,
+          amount: 3,
+          output: 1
+        }
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api'
+    this.get('/products', () => {
+      return this.schema.all('products')
+    })
+    this.post('products', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('products', data) //verificar se não é products ao invez de product
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,10 @@
+import { api } from '../../services/api'
 import { Container } from './style'
 
 export function Home() {
+  //consumindo api
+  api.get('products').then(response => console.log(response))
+
   return (
     <Container>
       <h2> Hello World</h2>
